a17342a719c35443eae90bc2a67e329c358655b1
Tina Maria Thomas <tinainink@gmail.com>
2020-11-27 22:46:50 -0500

    Testing reduced coverage

--- detector/filecontent/base64_aggressive_detector_test.go
+++ detector/filecontent/base64_aggressive_detector_test.go
@@ -11,28 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestShouldFlagPotentialAWSAccessKeysInAggressiveMode(t *testing.T) {
-	const awsAccessKeyIDExample string = "AKIAIOSFODNN7EXAMPLE\n"
-	results := helpers.NewDetectionResults()
-	content := []byte(awsAccessKeyIDExample)
-	filename := "filename"
-	additions := []gitrepo.Addition{gitrepo.NewAddition(filename, content)}
-
-	NewFileContentDetector(talismanRC).AggressiveMode().Test(helpers.NewChecksumCompare(nil, utility.DefaultSHA256Hasher{}, talismanrc.NewTalismanRC(nil)), additions, talismanRC, results, func() {})
-	assert.True(t, results.HasFailures(), "Expected file to not to contain base64 encoded texts")
-}
-
-func TestShouldFlagPotentialAWSAccessKeysAtPropertyDefinitionInAggressiveMode(t *testing.T) {
-	const awsAccessKeyIDExample string = "accessKey=AKIAIOSFODNN7EXAMPLE"
-	results := helpers.NewDetectionResults()
-	content := []byte(awsAccessKeyIDExample)
-	filename := "filename"
-	additions := []gitrepo.Addition{gitrepo.NewAddition(filename, content)}
-
-	NewFileContentDetector(talismanRC).AggressiveMode().Test(helpers.NewChecksumCompare(nil, utility.DefaultSHA256Hasher{}, talismanrc.NewTalismanRC(nil)), additions, talismanRC, results, func() {})
-	assert.True(t, results.HasFailures(), "Expected file to not to contain base64 encoded texts")
-}
-
 func TestShouldNotFlagPotentialSecretsWithinSafeJavaCodeEvenInAggressiveMode(t *testing.T) {
 	const awsAccessKeyIDExample string = "public class HelloWorld {\r\n\r\n    public static void main(String[] args) {\r\n        // Prints \"Hello, World\" to the terminal window.\r\n        System.out.println(\"Hello, World\");\r\n    }\r\n\r\n}"
 	results := helpers.NewDetectionResults()
--- detector/filecontent/base64_detector_test.go
+++ detector/filecontent/base64_detector_test.go
@@ -14,21 +14,3 @@ func TestBase64DetectorShouldNotDetectSafeText(t *testing.T) {
 	res := bd.CheckBase64Encoding(s)
 	assert.Equal(t, "", res)
 }
-
-func TestBase64DetectorShouldDetectBase64Text(t *testing.T) {
-	s := "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-	bd := Base64Detector{}
-	bd.initBase64Map()
-
-	res := bd.CheckBase64Encoding(s)
-	assert.Equal(t, s, res)
-}
-
-func TestBase64DetectorShouldNotDetectLongMethodNamesEvenWithHighEntropy(t *testing.T) {
-	s := "TestBase64DetectorShouldNotDetectLongMethodNamesEvenWithRidiculousHighEntropyWordsMightExist"
-	bd := Base64Detector{}
-	bd.initBase64Map()
-
-	res := bd.CheckBase64Encoding(s)
-	assert.Equal(t, "", res)
-}
